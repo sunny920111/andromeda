95ea811dbad77fadaf8d14c6fd94de2db3e916fc
sunny920111 <sunny920111@naver.com>
2014-11-17 19:16:00 +0900

    test

---
+++ Macro/src/com/example/macro/MacroService.java
@@ -0,0 +1,167 @@
+package com.example.macro;
+import android.app.Instrumentation;
+import android.app.Service;
+import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Looper;
+import android.os.SystemClock;
+import android.view.Gravity;
+import android.view.InputDevice;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MacroService extends Service implements OnClickListener,Runnable{
+
+	
+    private WindowManager.LayoutParams mParams;  //layout params 객체. 뷰의 위치 및 크기
+	private WindowManager mWindowManager;  
+	private LayoutInflater inflater;
+	private View myView;
+	private boolean flag =true;
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void onCreate(){
+		super.onCreate();
+
+		inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+		myView = inflater.inflate(R.layout.activity_main, null);     
+		
+		mParams = new WindowManager.LayoutParams(
+					WindowManager.LayoutParams.WRAP_CONTENT,
+		            WindowManager.LayoutParams.WRAP_CONTENT,
+		            WindowManager.LayoutParams.TYPE_PHONE,//항상 최 상위. 터치 이벤트 받을 수 있음.
+		            WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,  //포커스를 가지지 않음
+		            PixelFormat.TRANSLUCENT
+		);
+		
+		mParams.gravity = Gravity.LEFT | Gravity.TOP;
+	    mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+	    mWindowManager.addView(myView, mParams);
+	   
+	    setButtonListener(myView);
+	    
+	}
+
+	@Override
+	public void onDestroy(){
+
+		if(mWindowManager != null) {        //서비스 종료시 뷰 제거. *중요 : 뷰를 꼭 제거 해야함.
+			if(myView != null){
+				((WindowManager) getSystemService(WINDOW_SERVICE)).removeView(myView);
+
+			}
+            
+            mWindowManager = null;
+        }
+		super.onDestroy();
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()){
+		case R.id.start : 
+			startMacro();
+			break;
+		case R.id.stop :
+			stopMacro();
+			break;
+		}
+	}
+	
+	public void setButtonListener(View myView){
+		Button startButton = (Button)myView.findViewById(R.id.start);
+		Button stopButton = (Button)myView.findViewById(R.id.stop);
+		
+		startButton.setOnClickListener(this);
+		stopButton.setOnClickListener(this);
+		
+	}
+	
+	public void startMacro(){
+		System.out.println("startMacro");
+		flag = true;
+		this.run();
+	}
+	
+	public void stopMacro(){
+
+		System.out.println("stopMacro");
+		flag = false;
+	}
+	
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+			try{
+				long downTime = SystemClock.uptimeMillis();
+				long eventTime = SystemClock.uptimeMillis();
+				final MotionEvent down_event = MotionEvent.obtain(downTime, eventTime,   MotionEvent.ACTION_DOWN, 100,100, 0);
+				final MotionEvent up_event = MotionEvent.obtain(downTime, eventTime,   MotionEvent.ACTION_UP, 100, 100, 0);
+				down_event.setSource(InputDevice.SOURCE_TOUCHSCREEN);		
+				up_event.setSource(InputDevice.SOURCE_TOUCHSCREEN);
+				
+		
+				final Instrumentation inst = new Instrumentation() ;
+				Thread task = new Thread(new Runnable() {
+					 private Handler handler;
+		
+					@Override
+					public void run() {
+						// TODO Auto-generated method stub
+						 Looper.prepare();
+		                 handler = new Handler();
+		                 handler.post(new Runnable() {
+		
+							@Override
+							public void run() {
+								// TODO Auto-generated method stub
+								while(flag){
+									
+			                         try {
+			                        	 inst.sendPointerSync(down_event);
+			                             Thread.sleep(10);
+			                             inst.sendPointerSync(up_event);
+			                             Thread.sleep(200);
+			                          } catch (InterruptedException e) {
+			                             // TODO Auto-generated catch block
+			                             e.printStackTrace();
+			                         }
+			                   
+								}
+								
+							}
+		                 
+		                 });
+		                 
+		                 Looper.loop();
+		
+					}
+					
+					
+				});
+				
+				 task.start();
+			
+			
+			}catch(NullPointerException e){
+				e.printStackTrace();
+			}
+	
+	}
+	
+	
+
+}
---
+++ Macro/src/com/example/macro/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.macro;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class MainActivity extends Activity implements OnClickListener {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_layout);
+	
+		findViewById(R.id.startAppButton).setOnClickListener(this);
+		findViewById(R.id.stopAppButton).setOnClickListener(this);
+		
+
+	}
+	
+	public void onClick(View v){
+		  int view = v.getId();
+		  
+	      if(view == R.id.startAppButton){
+	    	  startService(new Intent(this, MacroService.class));    //서비스 시작  
+	      } else{
+	    	  stopService(new Intent(this, MacroService.class));    //서비스 종료
+	      }
+	          
+	    
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
