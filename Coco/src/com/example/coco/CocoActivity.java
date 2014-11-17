fe5a445c9bdea033844dcf3292329215eae554f7
sunny920111 <sunny920111@naver.com>
2014-11-17 14:27:32 +0900

    ghfgh

--- Coco/src/com/example/coco/CocoActivity.java
+++ Coco/src/com/example/coco/CocoActivity.java
@@ -8,47 +8,51 @@ import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
+//tab layout을 구현하기 위해서는 FragmentActivity와 Fragment가 필요하다.
+//FragmentActivity은 하나의 틀이고, Fragment는 각 탭 화면에 대한 상세설계라고 생각하면 된다.
 public class CocoActivity extends FragmentActivity implements ActionBar.TabListener {
+	
+	//각 탭에 대한 레이아웃을 지정할 수 있는 class , 우리가 정의해야 한다. 
 	AppSectionsPagerAdapter mAppSectionsPagerAdapter;
+	
+	//수평으로  view를 좌우로 스크롤할때 사용하는 class , 라이브러리에서 제공 
     ViewPager mViewPager;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		
+		/*
+		 * onCreate할때 Bundle을 저장하는데 이는프로세스가 메모리부족등으로
+		 * kill된 후에 다시 복원 될때, 해당하는 savedInstance 정보를 가지고 자동으로 복원해준다. 
+		 * */
 		super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_main);
-	 // Create the adapter that will return a fragment for each of the three primary sections
-        // of the app.
+	
+	    
+	    //각 탭을 정의하고, 탭에 대한 해당하는 레이아웃과 기능을 정의한다. 
+	    //getSupportFragmentManager은 FragmentActivity 각 fragment을 관리할 수 있는 manager를 return해준다. 
         mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
 
-        // Set up the action bar.
+        // action Bar를 선언한다. 
         final ActionBar actionBar = getActionBar();
 
-        // Specify that the Home/Up button should not be enabled, since there is no hierarchical
-        // parent.
         actionBar.setHomeButtonEnabled(false);
 
-        // Specify that we will be displaying tabs in the action bar.
+        // display mode를 선언한다. 
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        // Set up the ViewPager, attaching the adapter and setting up a listener for when the
-        // user swipes between sections.
+        //activity_main에서 선언한 viewpager를 id를 통해서 가져온다.
         mViewPager = (ViewPager) findViewById(R.id.pager);
         mViewPager.setAdapter(mAppSectionsPagerAdapter);
+        
+       //각 탭이 이동할때, action에 애해 정의를 해준다.
         mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
             public void onPageSelected(int position) {
-                // When swiping between different app sections, select the corresponding tab.
-                // We can also use ActionBar.Tab#select() to do this if we have a reference to the
-                // Tab.
                 actionBar.setSelectedNavigationItem(position);
             }
         });
 
         // For each of the sections in the app, add a tab to the action bar.
         for (int i = 0; i < mAppSectionsPagerAdapter.getCount(); i++) {
-            // Create a tab with text corresponding to the page title defined by the adapter.
-            // Also specify this Activity object, which implements the TabListener interface, as the
-            // listener for when this tab is selected.
             actionBar.addTab(
                     actionBar.newTab()
                             .setText(mAppSectionsPagerAdapter.getPageTitle(i))
