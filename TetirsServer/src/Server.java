d11020b1b08c7fdffb59ac1d51fd36cf98c6944e
sunny920111 <sunny920111@naver.com>
2014-12-05 15:39:28 +0900

    e

--- TetirsServer/src/Server.java
+++ TetirsServer/src/Server.java
@@ -8,7 +8,7 @@ public class Server {
 	        Socket socket = null;
 	 
 	        try {
-	            serverSocket = new ServerSocket(7777);
+	            serverSocket = new ServerSocket(9000);
 	            System.out.println("서버가 준비되었습니다.");
 	 
 	            socket = serverSocket.accept();
